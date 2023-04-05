021b47f732bed9140e18f2ae8b13c714f6fad82f
KINastya <kilusiknastya@gmail.com>
2023-04-05 19:10:37 +0300

    created homework 11

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const removeElement = (array, item) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
+};
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+removeElement (array, 7);
+console.log(array);
+
